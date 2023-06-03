b7e23cb13edd249fa68a269300511e1b0b591037
poafs1 <prach.yot@gmail.com>
2023-06-03 23:00:12 +0700

    feat: add default style to button

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { ButtonHTMLAttributes, ReactNode } from 'react';
 
-export interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   leadingIcon?: ReactNode;
   trailingIcon?: ReactNode;
   label?: string;
@@ -11,41 +11,122 @@ export interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   buttonSize: 'sm' | 'base' | 'lg';
 }
 
-export const Button = (props: IButtonProps) => {
-  const {
-    leadingIcon,
-    trailingIcon,
-    label,
-    onClick,
-    link,
-    disabled,
-    // buttonWidth,
-    type,
-    form,
-    // buttonStyle,
-  } = props;
+export class Button extends Component<IButtonProps> {
+  static defaultProps: Partial<IButtonProps> = {
+    buttonStyle: 'primary',
+    buttonSize: 'base',
+    buttonWidth: 'auto',
+  };
 
-  const renderButton = () => {
+  state = {
+    isHovered: false,
+    isActive: false,
+  };
+
+  handleMouseEnter = () => {
+    this.setState({ isHovered: true });
+  };
+
+  handleMouseLeave = () => {
+    this.setState({ isHovered: false });
+  };
+
+  handleMouseDown = () => {
+    this.setState({ isActive: true });
+  };
+
+  handleMouseUp = () => {
+    this.setState({ isActive: false });
+  };
+
+  renderButton = () => {
+    const { leadingIcon, trailingIcon, label } = this.props;
+
+    const { isHovered, isActive } = this.state;
+
     return (
-      <div>
-        <span>{leadingIcon}</span>
-        {label && <span>{label}</span>}
-        <span>{trailingIcon}</span>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          backgroundColor: isActive ? '#f3f4f6' : isHovered ? '#d1d5db' : '#e5e7eb',
+          borderRadius: '0.25rem',
+          padding: '0.25rem 0.5rem',
+          fontSize: '0.875rem',
+        }}
+      >
+        <span
+          style={{
+            display: 'grid',
+            placeItems: 'center',
+          }}
+        >
+          {leadingIcon}
+        </span>
+        {label && (
+          <span
+            style={{
+              marginLeft: '0.5rem',
+            }}
+          >
+            {label}
+          </span>
+        )}
+        <span
+          style={{
+            display: 'grid',
+            placeItems: 'center',
+            marginLeft: '0.5rem',
+          }}
+        >
+          {trailingIcon}
+        </span>
       </div>
     );
   };
 
-  return link ? (
-    <a href={link}>{renderButton()}</a>
-  ) : (
-    <button onClick={onClick} disabled={disabled} type={type} form={form}>
-      {renderButton()}
-    </button>
-  );
-};
+  render() {
+    const { onClick, link, disabled, buttonWidth, type, form } = this.props;
 
-Button.defaultProps = {
-  buttonStyle: 'primary',
-  buttonSize: 'md',
-  buttonWidth: 'auto',
-};
+    if (link) {
+      return (
+        <a
+          href={link}
+          style={{
+            display: 'inline-block',
+            width: buttonWidth === 'full' ? '100%' : buttonWidth,
+          }}
+          onMouseEnter={this.handleMouseEnter}
+          onMouseLeave={this.handleMouseLeave}
+          onMouseDown={this.handleMouseDown}
+          onMouseUp={this.handleMouseUp}
+        >
+          {this.renderButton()}
+        </a>
+      );
+    }
+
+    return (
+      <button
+        onClick={onClick}
+        disabled={disabled}
+        type={type}
+        form={form}
+        style={{
+          width: buttonWidth === 'full' ? '100%' : buttonWidth,
+          backgroundColor: 'transparent',
+          border: 'none',
+          outline: 'none',
+          cursor: 'pointer',
+          padding: 0,
+        }}
+        onMouseEnter={this.handleMouseEnter}
+        onMouseLeave={this.handleMouseLeave}
+        onMouseDown={this.handleMouseDown}
+        onMouseUp={this.handleMouseUp}
+      >
+        {this.renderButton()}
+      </button>
+    );
+  }
+}
